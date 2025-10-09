f770929b6fdb4a15b32f27127cefac1c3a006f60
ateesh3009 <ateeshbala@gmail.com>
2025-10-09 15:54:11 +0530

    Add files via upload

---
+++ Home task4.py
@@ -0,0 +1,14 @@
+#Homework task-1
+num1 = int(input("enter your first number="))
+num2 = int(input("enter your second number="))
+ateesh = num1 * num2
+if ateesh % 15 == 0:
+ print("the ateesh is", ateesh)
+else:
+ print("it is not divisble")
+
+#Homework task-2
+text = input("Enter a string: ")
+reversed_text = text[::-1]
+print("Reversed string:", reversed_text)
+
